02d23c04463b8ff761567a2af61965036a4fdfb0
sergyops <70977028+sergyops@users.noreply.github.com>
2022-11-26 17:57:03 +0100

    Agregar archivos de proyecto.

---
+++ ConexionBBDD/Methods.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConexionBBDD
+{
+    class Methods
+    {
+        public void ShowCliente(string query, SqlConnection conn)
+        {
+            using (SqlCommand command = new SqlCommand(query, conn))
+            {
+                command.CommandType = CommandType.Text;
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("=====================");
+                        Console.WriteLine("ID " + reader["id"].ToString());
+                        Console.WriteLine("Nombre " + reader["nombre"].ToString());
+                        Console.WriteLine("Apellido " + reader["apellido"].ToString());
+                        Console.WriteLine("N.Articulos " + reader["narticulos"].ToString());
+                        Console.WriteLine("Dinero gastado " + reader["dingastado"].ToString());
+                    }
+                }
+            }
+        }
+
+        public void ShowArticulo(string query, SqlConnection conn)
+        {
+            using (SqlCommand command = new SqlCommand(query, conn))
+            {
+                command.CommandType = CommandType.Text;
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("=====================");
+                        Console.WriteLine("ID " + reader["id"].ToString());
+                        Console.WriteLine("Nombre " + reader["nombre"].ToString());
+                        Console.WriteLine("Precio " + reader["precio"].ToString());
+                        Console.WriteLine("Seccion " + reader["seccion"].ToString());
+                        Console.WriteLine("N.Articulos " + reader["narticulos"].ToString());
+                    }
+                }
+            }
+        }
+
+        public void executeUpdate(string query, SqlConnection conn)
+        {
+            using (SqlCommand command = new SqlCommand(query, conn))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void executeInsert(string query, SqlConnection conn)
+        {
+            using (SqlCommand command = new SqlCommand(query, conn))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ ConexionBBDD/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data;
+using System.Text;
+using ConexionBBDD;
+using Microsoft.Data.SqlClient;
+
+namespace Connection
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string datasource = "DESKTOP-GM5SGO2";
+            string database = "Comercio";//initial catalog
+            string username = "sa";
+            string password = "adm1n";
+
+            string connString = @"Data Source=" + datasource + ";Initial Catalog="
+                        + database + ";Persist Security Info=True;User ID=" + username + ";Password=" + password + ";TrustServerCertificate=true";
+
+            SqlConnection conn = new SqlConnection(connString);//nueva conexión
+            Methods metodo = new Methods();
+
+            try
+            {
+                conn.Open();
+
+                //INSERT
+                metodo.executeInsert("INSERT INTO cliente (id, nombre, apellido, narticulos, dingastado) VALUES (11, 'pepe', 'garcia', 30, 400)",conn);
+                
+                Console.WriteLine("imprimimos los datos que hemos introducido");
+                metodo.ShowCliente("SELECT * FROM cliente WHERE id = '11'", conn);
+
+
+                //UPDATE
+                metodo.executeUpdate("UPDATE cliente SET apellido = 'soria' WHERE id = '11'", conn);
+
+                Console.WriteLine("imprimimos para comprobar que han cambiado los datos");
+                metodo.ShowCliente("SELECT * FROM cliente WHERE id = '11'", conn);
+
+
+                //SELECT
+                Console.WriteLine("imprimimos los datos de la tabla articulo");
+                metodo.ShowArticulo("SELECT * FROM articulo", conn);
+
+                conn.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
